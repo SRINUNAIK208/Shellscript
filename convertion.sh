eec64d03339fca902cdd33af1b8f87eb34e190fd
SRINUNAIK208 <srinaik208@gmail>
2025-12-29 15:26:40 +0530

    convertion

--- convertion.sh
+++ convertion.sh
@@ -4,4 +4,4 @@
 echo "Trump: Hey Mask, how are you?"
 echo "Mask: Hello Trump, i am good. what about you?"
 echo "Trump: i am good, when did you came to india?"
-echo "Mask: i came two days back with my daughter.
+echo "Mask: i came two days back with my daughter."
