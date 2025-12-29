a298450fa95d7f89ae55add83590fec4ca9b36a2
SRINUNAIK208 <srinaik208@gmail>
2025-12-29 15:33:22 +0530

    variables

---
+++ variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+Person1=Trump
+Person2=Musk
+
+echo "$Person1: Hey $Person2, how are you?"
+echo "$Person2: Hello $Person1, i am good. what about you?"
+echo "$Person1: i am good, when did you came to india?"
+echo "$Person2: i came two days back with my daughter."
